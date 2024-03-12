1dc11d178e30d9a9ba3d9e021d850f117249aee9
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-03-12 11:19:43 -0500

    arithmetic.md

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+function getRandomInt() {
+  return Math.floor(Math.random() * 100) + 1; 
+}
+const random = getRandomInt();
+
+const num3 = 20;
+const num4 = 16;
+const mod = num3 % num4; 
+
+const numbers = [10, 20, 15, 5];
+const max = Math.max(...numbers);
+
+console.log(`multiply: ${multiply}, random: ${random}, mod: ${mod}, max: ${max}`);
